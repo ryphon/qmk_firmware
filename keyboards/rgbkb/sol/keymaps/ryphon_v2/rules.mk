a5d8fdbf59827c5a141125a195abee8ef18dcf53
Ryphon <dylan@shepard.dev>
2019-12-27 22:04:30 -0700

    hmm

--- keyboards/rgbkb/sol/keymaps/ryphon_v2/rules.mk
+++ keyboards/rgbkb/sol/keymaps/ryphon_v2/rules.mk
@@ -9,7 +9,3 @@ RGB_MATRIX_FRAMEBUFFER_EFFECTS = yes # Enable frame buffer effects like the typi
 # Misc
 OLED_DRIVER_ENABLE      = yes       # Enable the OLED Driver
 LINK_TIME_OPTIMIZATION_ENABLE = yes # Enable Link Time Optimizations greatly reducing firmware size by disabling the old Macros and Functions features
-
-# Special RGB Matrix, OLED, & Encoder Control Menu!
-RGB_OLED_MENU           = yes     # Enabled by setting this to the encoder index (0-5) you wish to use to control the menu.
-                                # Use the RGB_MENU keycode in the keymap for the encoder to advance the menu to the next option.
