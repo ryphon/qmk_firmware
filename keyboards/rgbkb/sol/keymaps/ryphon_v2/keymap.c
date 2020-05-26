9e9534e7fd1bcb8b2597a11803ab5b0f86fe0bad
Dylan Shepard <dylan@shepard.dev>
2020-05-26 07:22:38 -0700

    reversing encoder

--- keyboards/rgbkb/sol/keymaps/ryphon_v2/keymap.c
+++ keyboards/rgbkb/sol/keymaps/ryphon_v2/keymap.c
@@ -95,10 +95,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RGB] =  LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______, _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, \
-    _______, RGB_SAD, RGB_VAI, RGB_SAI,   RESET, _______, _______, _______, _______,    KC_U,    KC_I,    KC_O, _______, _______, \
-    _______, RGB_HUD, RGB_VAD, RGB_HUI,  RGBRST, _______, _______, _______, _______,    KC_J,    KC_K,    KC_L, _______, _______,\
-    _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, RGB_SAD, RGB_VAI, RGB_SAI,   RESET, _______, _______, _______, KC_NLCK, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, \
+    _______, RGB_HUD, RGB_VAD, RGB_HUI,  RGBRST, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,\
+    _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, \
+    _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, KC_KP_0, KC_PDOT, KC_PENT, _______, _______, \
+
                                                  _______, _______, _______, _______ \
   )
 };
@@ -171,18 +172,18 @@ typedef void (*rgb_f)(void);
 
 const rgb_f rgb_functions[RGB_FUNCTION_COUNT][2] = {
 #if defined(RGB_MATRIX_ENABLE)
-    { rgb_matrix_decrease_hue   ,   rgb_matrix_increase_hue   },
-    { rgb_matrix_decrease_sat   ,   rgb_matrix_increase_sat   },
-    { rgb_matrix_decrease_val   ,   rgb_matrix_increase_val   },
-    { rgb_matrix_decrease_speed ,   rgb_matrix_increase_speed },
-    { rgb_matrix_step_reverse   ,   rgb_matrix_step           },
-    { rgb_matrix_decrease_flags ,   rgb_matrix_increase_flags }
+    {  rgb_matrix_increase_hue   ,  rgb_matrix_decrease_hue    },
+    {  rgb_matrix_increase_sat   ,  rgb_matrix_decrease_sat    },
+    {  rgb_matrix_increase_val   ,  rgb_matrix_decrease_val    },
+    {  rgb_matrix_increase_speed ,  rgb_matrix_decrease_speed  },
+    {  rgb_matrix_step           ,  rgb_matrix_step_reverse    },
+    {  rgb_matrix_increase_flags ,  rgb_matrix_decrease_flags  }
 #elif defined(RGBLIGHT_ENABLE)
-    { rgblight_decrease_hue     ,   rgblight_increase_hue     },
-    { rgblight_decrease_sat     ,   rgblight_increase_sat     },
-    { rgblight_decrease_val     ,   rgblight_increase_val     },
-    { rgblight_decrease_speed   ,   rgblight_increase_speed   },
-    { rgblight_step_reverse     ,   rgblight_step             }
+    {  rgblight_increase_hue     ,  rgblight_decrease_hue      },
+    {  rgblight_increase_sat     ,  rgblight_decrease_sat      },
+    {  rgblight_increase_val     ,  rgblight_decrease_val      },
+    {  rgblight_increase_speed   ,  rgblight_decrease_speed    },
+    {  rgblight_step             ,  rgblight_step_reverse      }
 #endif
 };
 
@@ -268,8 +269,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         if (get_mods() & MOD_MASK_SHIFT) {
           rgb_encoder_state = (rgb_encoder_state - 1);
-          if (rgb_encoder_state > 5) { rgb_encoder_state = 5;
-          }
+          if (rgb_encoder_state > 5) {
+		  rgb_encoder_state = 5;
+	  }
         } else {
           rgb_encoder_state = (rgb_encoder_state + 1) % 6;
         }
