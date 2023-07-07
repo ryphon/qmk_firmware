d3d725c95919e68512265d227747a6171d1cc358
Dylan Shepard <dylan@shepard.dev>
2023-07-07 13:00:51 -0600

    updating

--- keyboards/rgbkb/sol/keymaps/ryphon_v2/keymap.c
+++ keyboards/rgbkb/sol/keymaps/ryphon_v2/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                    `-------------'                     `-------------'
    */
   [_QWERTY] = LAYOUT( \
-    KC_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,  KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
+    QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_MINS,  KC_EQL,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, \
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC, KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS, \
          FN,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, _______, _______,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, \
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, _______, _______,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, FN_CAPS, \
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_FN] = LAYOUT( \
      KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, _______, _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, \
-    _______, KC_PGDN,   KC_UP, KC_PGUP, _______, _______, KC_NLCK, KC_SLCK, _______, KC_PGDN,   KC_UP, KC_PGUP, KC_PSCR,  KC_F12, \
+    _______, KC_PGDN,   KC_UP, KC_PGUP, _______, _______,  KC_NUM, KC_SCRL, _______, KC_PGDN,   KC_UP, KC_PGUP, KC_PSCR,  KC_F12, \
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______,  KC_INS, \
     _______, _______, _______, _______, _______, _______, _______, _______,  KC_END, _______, _______, _______, _______, KC_VOLU, \
     _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, \
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RGB] =  LAYOUT( \
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______, _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, \
-    _______, RGB_SAD, RGB_VAI, RGB_SAI,   RESET, _______, _______, _______, KC_NLCK, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, \
+    _______, RGB_SAD, RGB_VAI, RGB_SAI, QK_BOOT, _______, _______, _______,  KC_NUM, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______, \
     _______, RGB_HUD, RGB_VAD, RGB_HUI,  RGBRST, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,\
     _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______, \
     _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, KC_KP_0, KC_PDOT, KC_PENT, _______, _______, \
@@ -162,37 +162,22 @@ void rgb_matrix_decrease_flags(void)
 #ifdef RGB_OLED_MENU
 uint8_t rgb_encoder_state = 4;
 
-#if defined(RGB_MATRIX_ENABLE)
-    #define RGB_FUNCTION_COUNT 6
-#elif defined(RGBLIGHT_ENABLE)
-    #define RGB_FUNCTION_COUNT 5
-#endif
+typedef void (*rgb_matrix_f)(void);
 
-typedef void (*rgb_f)(void);
-
-const rgb_f rgb_functions[RGB_FUNCTION_COUNT][2] = {
-#if defined(RGB_MATRIX_ENABLE)
-    {  rgb_matrix_increase_hue   ,  rgb_matrix_decrease_hue    },
-    {  rgb_matrix_increase_sat   ,  rgb_matrix_decrease_sat    },
-    {  rgb_matrix_increase_val   ,  rgb_matrix_decrease_val    },
-    {  rgb_matrix_increase_speed ,  rgb_matrix_decrease_speed  },
-    {  rgb_matrix_step           ,  rgb_matrix_step_reverse    },
-    {  rgb_matrix_increase_flags ,  rgb_matrix_decrease_flags  }
-#elif defined(RGBLIGHT_ENABLE)
-    {  rgblight_increase_hue     ,  rgblight_decrease_hue      },
-    {  rgblight_increase_sat     ,  rgblight_decrease_sat      },
-    {  rgblight_increase_val     ,  rgblight_decrease_val      },
-    {  rgblight_increase_speed   ,  rgblight_decrease_speed    },
-    {  rgblight_step             ,  rgblight_step_reverse      }
-#endif
+const rgb_matrix_f rgb_matrix_functions[6][2] = {
+    { rgb_matrix_increase_hue, rgb_matrix_decrease_hue },
+    { rgb_matrix_increase_sat, rgb_matrix_decrease_sat },
+    { rgb_matrix_increase_val, rgb_matrix_decrease_val },
+    { rgb_matrix_increase_speed, rgb_matrix_decrease_speed },
+    { rgb_matrix_step, rgb_matrix_step_reverse },
+    { rgb_matrix_increase_flags, rgb_matrix_decrease_flags }
 };
-
 #endif
 
 #ifdef ENCODER_ENABLE
 
 static pin_t encoders_pad_a[] = ENCODERS_PAD_A;
-#define NUMBER_OF_ENCODERS (sizeof(encoders_pad_a)/sizeof(pin_t))
+#define NUMBER_OF_ENCODERS ARRAY_SIZE(encoders_pad_a)
 
 const uint16_t PROGMEM encoders[][NUMBER_OF_ENCODERS * 2][2]  = {
     [_QWERTY] = ENCODER_LAYOUT( \
@@ -209,17 +194,17 @@ const uint16_t PROGMEM encoders[][NUMBER_OF_ENCODERS * 2][2]  = {
     )
 };
 
-void encoder_update_user(uint8_t index, bool clockwise) {
+bool encoder_update_user(uint8_t index, bool clockwise) {
   if (!is_keyboard_master())
-    return;
+    return true;
 
 #ifdef RGB_OLED_MENU
   if (index == RGB_OLED_MENU) {
-    (*rgb_functions[rgb_encoder_state][clockwise])();
+    (*rgb_matrix_functions[rgb_encoder_state][clockwise])();
   } else
 #endif
   {
-    uint8_t layer = biton32(layer_state);
+    uint8_t layer = get_highest_layer(layer_state);
     uint16_t keycode = pgm_read_word(&encoders[layer][index][clockwise]);
     while (keycode == KC_TRANSPARENT && layer > 0)
     {
@@ -230,6 +215,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
     if (keycode != KC_TRANSPARENT)
       tap_code16(keycode);
   }
+    return true;
 }
 #endif
 
@@ -248,7 +234,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
 #endif
       return false;
-    case RESET:
+    case QK_BOOT:
       if (record->event.pressed) {
           reset_timer = timer_read();
       } else {
@@ -270,8 +256,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (get_mods() & MOD_MASK_SHIFT) {
           rgb_encoder_state = (rgb_encoder_state - 1);
           if (rgb_encoder_state > 5) {
-		  rgb_encoder_state = 5;
-	  }
+		    rgb_encoder_state = 5;
+	      }
         } else {
           rgb_encoder_state = (rgb_encoder_state + 1) % 6;
         }
@@ -283,7 +269,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 // OLED Driver Logic
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (is_keyboard_master())
     return OLED_ROTATION_270;
@@ -310,7 +296,7 @@ static void render_status(void) {
 
   // Define layers here
   oled_write_P(PSTR("Layer"), false);
-  uint8_t layer = layer_state ? biton(layer_state) : biton32(default_layer_state);
+  uint8_t layer = get_highest_layer(layer_state|default_layer_state);
   switch (layer) {
     case _QWERTY:
       oled_write_P(PSTR("BASE "), false);
@@ -342,13 +328,14 @@ static void render_status(void) {
 #endif
 }
 
-void oled_task_user(void) {
+bool oled_task_user(void) {
   if (is_keyboard_master()) {
     render_status();
   } else {
     render_logo();
     oled_scroll_left();
   }
+    return false;
 }
 
 #endif
--- keyboards/rgbkb/sol/keymaps/ryphon_v2/rules.mk
+++ keyboards/rgbkb/sol/keymaps/ryphon_v2/rules.mk
@@ -1,11 +1,1 @@
-# Keycode Options
-EXTRAKEY_ENABLE         = yes       # Audio control and System control
-
-# RGB
-RGB_MATRIX_ENABLE       = WS2812     # Enable per-key coordinate based RGB effects. Do not enable with RGBlight
-RGB_MATRIX_KEYPRESSES   = yes        # Enable reactive per-key effects.
-RGB_MATRIX_FRAMEBUFFER_EFFECTS = yes # Enable frame buffer effects like the typing heatmap.
-
-# Misc
-OLED_DRIVER_ENABLE      = yes       # Enable the OLED Driver
-LINK_TIME_OPTIMIZATION_ENABLE = yes # Enable Link Time Optimizations greatly reducing firmware size by disabling the old Macros and Functions features
+include keyboards/$(KEYBOARD)/post_rules.mk
