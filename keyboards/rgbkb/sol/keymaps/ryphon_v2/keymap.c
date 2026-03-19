65001841bdb32bcc3bd09a0bb15aad487722324d
ryphon <dylan@shepard.dev>
2026-03-18 21:36:02 -0600

    as built

--- keyboards/rgbkb/sol/keymaps/ryphon_v2/keymap.c
+++ keyboards/rgbkb/sol/keymaps/ryphon_v2/keymap.c
@@ -7,13 +7,13 @@
 enum layer_number {
     _QWERTY = 0,
     _FN,
-    _RGB
+    _SYSCTL
 };
 
 // Keycode defines for layers
 #define QWERTY   DF(_QWERTY)
 #define FN       MO(_FN)
-#define RGB      MO(_RGB)
+#define SYSCTL   MO(_SYSCTL)
 
 enum custom_keycodes {
   RGBRST = SAFE_RANGE,
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_LBRC, KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
          FN,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G, _______, _______,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, _______, _______,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, FN_CAPS,
-    KC_LCTL,      FN,     RGB, KC_LGUI, KC_LALT,  KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC,RGB_MENU, KC_MPRV, KC_MPLY, KC_MNXT, KC_RCTL,
+    KC_LCTL,      FN,  SYSCTL, KC_LGUI, KC_LALT,  KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC,RGB_MENU, KC_MPRV, KC_MPLY, KC_MNXT, KC_RCTL,
                                                   KC_SPC,  KC_DEL,  KC_ENT,  KC_SPC
   ),
 
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_PGDN,   KC_UP, KC_PGUP, _______, _______,  KC_NUM, KC_SCRL, _______, KC_PGDN,   KC_UP, KC_PGUP, KC_PSCR,  KC_F12,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______,  KC_INS,
     _______, _______, _______, _______, _______, _______, _______, _______,  KC_END, _______, _______, _______, _______, KC_VOLU,
-    _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD,
                                                  _______, _______, _______, _______
   ),
 
@@ -88,12 +88,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                    `-------------'                     `-------------'
    */
 
-  [_RGB] =  LAYOUT(
+  [_SYSCTL] =  LAYOUT(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, _______, _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
-    _______, RGB_SAD, RGB_VAI, RGB_SAI, QK_BOOT, _______, _______, _______,  KC_NUM, KC_KP_7, KC_KP_8, KC_KP_9, _______, _______,
-    _______, RGB_HUD, RGB_VAD, RGB_HUI,  RGBRST, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, _______, _______,
-    _______, RGB_SPD, _______, RGB_SPI, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, _______, _______,
-    _______, _______, _______, RGB_MOD, _______, _______, _______, _______, _______, KC_KP_0, KC_PDOT, KC_PENT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
                                                  _______, _______, _______, _______
   )
 };
@@ -182,7 +182,7 @@ const uint16_t PROGMEM encoders[][NUMBER_OF_ENCODERS * 2][2]  = {
         _______, _______,
         _______, _______
     ),
-    [_RGB] = ENCODER_LAYOUT(
+    [_SYSCTL] = ENCODER_LAYOUT(
         _______, _______,
         _______, _______
     )
@@ -298,11 +298,11 @@ static void render_status(void) {
     case _FN:
       oled_write_P(PSTR("FN   "), false);
       break;
-    case _RGB:
-      oled_write_P(PSTR("RGB  "), false);
+    case _SYSCTL:
+      oled_write_P(PSTR("SCTL "), false);
       break;
     default:
-      oled_write_P(PSTR("WTF"), false);
+      oled_write_P(PSTR("WTF "), false);
   }
 
   // Host Keyboard LED Status
